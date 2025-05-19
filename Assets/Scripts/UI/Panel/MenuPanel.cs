3e48fcef6cb52ac77c1843737bb979690033223f
Planarians <lyplanarians@gmail.com>
2025-05-19 15:52:26 +0100

    fix cursor
    成功隐藏到默认鼠标

---
+++ Assets/Scripts/UI/Panel/MenuPanel.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MenuPanel : MonoBehaviour
+{
+
+    private MouseCursor mouseCursor;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mouseCursor = FindAnyObjectByType<MouseCursor>(FindObjectsInactive.Include);
+    }
+
+    private void OnEnable()
+    {
+        mouseCursor.DisableDefaultCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
